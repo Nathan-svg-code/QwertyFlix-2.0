b82e4609acab35ddbeaa83402cbc763a3cc135ab
Nathan <you@example.com>
2020-07-30 16:34:18 -0300

    Setup inicial do projeto

--- src/App.js
+++ src/App.js
@@ -12,26 +12,26 @@ function App() {
     <Menu/>
 
     <BannerMain
-    videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+    videoTitle={dadosiniciais.categorias[0].videos[0].titulo}
     url={dadosIniciais.categorias[0].videos[0].url}
     videoDescription={"O que é Front-end? Trabalhando na área"}
     />
 
     <Carousel
     ignoreFirstVideo
-    category={dadosIniciais.categorias[0]}    
+    category={dadosiniciais.categorias[0]}    
       />
 
       <Carousel
-      category={dadosIniciais.categorias[1]}
+      category={dadosiniciais.categorias[1]}
       />
       
       <Carousel
-      category={dadosIniciais.categorias[2]}
+      category={dadosiniciais.categorias[2]}
       />
 
       <Carousel
-      category={dadosIniciais.categorias[3]}
+      category={dadosiniciais.categorias[3]}
       />
     </div>
   );
