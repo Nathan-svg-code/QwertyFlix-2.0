d13eeb95d03633a56452b4d7fdc57c7054ce381a
Nathan <you@example.com>
2020-07-30 16:46:40 -0300

    Setup Inicial do projeto

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -4,13 +4,13 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://www.Qwerty.com.br/">
+        <img src="https://www.Qwerty.com.br/assets/img/Qwerty-logo-white.1570550707.svg" alt="Logo Qwerty" />
       </a>
       <p>
         Orgulhosamente criado durante a
         {' '}
-        <a href="https://www.alura.com.br/">
+        <a href="https://www.Qwerty.com.br/">
           Imersão React da Alura
         </a>
       </p>
